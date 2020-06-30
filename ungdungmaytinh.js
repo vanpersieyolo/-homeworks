0a3b2c04ea39f67a22d580b63db9cad68a84fdcc
DANG ANH VAN <you@example.com>
2020-06-30 23:36:14 +0700

    'van

---
+++ ungdungmaytinh.js
@@ -0,0 +1,36 @@
+function start() {
+    var result = 0;
+    var sothunhat = document.getElementById('sothu1').value;
+    var sothuhai = document.getElementById('sothu2').value;
+    sothuhai = parseInt(sothuhai);
+    sothunhat = parseInt(sothunhat);
+    ketqua=sothuhai+sothunhat;
+    alert("ket qua la: " +ketqua);
+}
+function start1() {
+    var result = 0;
+    var sothunhat = document.getElementById('sothu1').value;
+    var sothuhai = document.getElementById('sothu2').value;
+    sothuhai = parseInt(sothuhai);
+    sothunhat = parseInt(sothunhat);
+    ketqua=sothunhat-sothuhai;
+    alert("ket qua la: " +ketqua);
+}
+function start2() {
+    var result = 0;
+    var sothunhat = document.getElementById('sothu1').value;
+    var sothuhai = document.getElementById('sothu2').value;
+    sothuhai = parseInt(sothuhai);
+    sothunhat = parseInt(sothunhat);
+    ketqua=sothunhat*sothuhai;
+    alert("ket qua la: " +ketqua);
+}
+function start3() {
+    var result = 0;
+    var sothunhat = document.getElementById('sothu1').value;
+    var sothuhai = document.getElementById('sothu2').value;
+    sothuhai = parseInt(sothuhai);
+    sothunhat = parseInt(sothunhat);
+    ketqua=sothunhat/sothuhai;
+    alert("ket qua la: " +ketqua);
+}
